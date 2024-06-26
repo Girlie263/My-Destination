2c35866ddc98b8f9200ae4f720d6d9ee8a58c341
Girlie263 <149056886+Girlie263@users.noreply.github.com>
2024-06-26 14:23:29 +0200

    added functionality to the book button and clear form button

--- src/script.js
+++ src/script.js
@@ -11,3 +11,32 @@ function handleForm(event){
 
 let searchElement = document.querySelector('#search-form');
 searchElement.addEventListener('submit',handleForm);
+
+
+let btnClear = document.querySelector(".clearButton");
+let inputs = document.querySelectorAll("input");
+
+btnClear.addEventListener('click', ()=>{
+  inputs.forEach(input => input.value = '');
+});
+
+let now = new Date;
+console.log(now);
+
+let hour = now.getHours()
+let minute = now.getMinutes()
+let second = now.getSeconds()
+
+let formattedTime =`Current Time:<br />${hour}:${minute}:${second}`;
+formattedTime.innerHTML = document.querySelector('.time')
+
+console.log(formattedTime);
+
+function startBooking(){
+  alert("Booked");
+}
+
+
+let bookingButton = document.querySelector('#booking');
+bookingButton.addEventListener('click',startBooking);
+
